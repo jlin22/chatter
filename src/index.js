5912fac364d92ed030e9adccfa1e276d52032426
John Lin <jlin1674@gmail.com>
2019-01-07 18:41:23 -0500

    set up data flow from App -> MsgList

--- src/index.js
+++ src/index.js
@@ -3,22 +3,53 @@ import ReactDOM from 'react-dom';
 import './style.css';
 
 class Title extends React.Component {
-
+	render() {
+		return null;
+	}
 }
 
-class MessagesList extends React.Component {
-
+class MessageList extends React.Component {
+	render() {
+		return null;
+	}
 }
 
 class SendMessagesForm extends React.Component {
-
+	render() {
+		return null;
+	}
 }
 
 class App extends React.Component {
-
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: this.props.data
+		}
+	}
+	render() {
+		return (
+			<div className="app">
+				<Title />
+				<MessageList data={this.state.data}/>
+				<SendMessagesForm />
+			</div>
+		);
+	}
 }
 
+const dummyData = [
+	{
+		senderId: 'john',
+		text: 'why is programming fun?'
+	},
+	{
+		senderId: 'computer',
+		text: 'wrong question to ask.'
+	}
+];
+
 ReactDOM.render(
-	<App />,
+	<App data={dummyData}/>,
 	document.getElementById('root')
 );
